7838ff1846c870326391360581a3aedc9541f5af
Daniele Perse <danieleperse3@gmail.com>
2020-07-14 20:23:04 -0300

    exercicio 6 dia 4.2 finalizado

--- exercises/4_2/exercicio5.js
+++ exercises/4_2/exercicio5.js
@@ -1,6 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 var maior = 0;
-//Utilizando for, descubra qual o maior valor contido no array e imprima-o;
 
 for (index = 0; index < numbers.length; index++) {  
          if (numbers[index] > maior) {
