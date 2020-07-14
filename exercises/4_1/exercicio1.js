d8975c907977b8d3edaaefec293ae339d8ee4e8d
Daniele Perse <danieleperse3@gmail.com>
2020-07-14 11:26:03 -0300

    excluindo pasta exercicios

--- exercises/4_1/exercicio1.js
+++
@@ -1,9 +0,0 @@
-let a = 5;
-let b = 2;
-
-console.log("soma = " + a + b);
-console.log("subtração = " + a - b);
-console.log("multiplicação = " + a * b);
-console.log("divisão = " + a / b);
-console.log("modulo = " + a % b);
-
