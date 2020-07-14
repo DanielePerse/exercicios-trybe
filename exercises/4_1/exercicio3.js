a5b81a7a03c1fe446fc8d2d05c063a56dbebdc0d
Daniele Perse <danieleperse3@gmail.com>
2020-07-14 10:46:39 -0300

    exercicios de javascript - dia 4.1

---
+++ exercises/4_1/exercicio3.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 24;
+let c = 2;
+
+if (a > b && a >c) {
+    console.log("O maior número é " + a);
+} else if (b > a && b > c) {
+    console.log("O maior número é " + b);
+} else {
+    console.log("O maior número é " + c);
+}
