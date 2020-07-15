81621714d10eaeda680e4660a54edbdd70949427
Daniele Perse <danieleperse3@gmail.com>
2020-07-15 13:38:54 -0300

    tentativa de fazer o bonus 1

---
+++ exercises/4_2/bonus1.js
@@ -0,0 +1,22 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//Ordene o array numbers em ordem crescente e imprima seus valores;
+//1.declarar e variavel, uma para resultado final e outra para comparar
+//2.fazer um for para percorrer o array
+//3.comparar os arrays
+//4.retirar o menor array encontrado da lista
+//4.guardar os resultados em outro array
+let comparacao = 0;
+let crescente = [];
+
+
+
+function ordemCrescente(a, b){
+    return a - b;
+}
+
+for (var i = 0; i < numbers.length; i +=1) {
+    
+}
+numbers.sort(ordemCrescente);
+
+
