fc633d59a66c3ee8aee12bea859490b6d1f5472b
Daniele Perse <danieleperse3@gmail.com>
2020-07-15 15:37:37 -0300

    exercicio de javascript dia 4.3

--- exercices/4_3/exercicio1.js
+++ exercices/4_3/exercicio1.js
@@ -1,1 +1,12 @@
-primeiro exercicio
+let n = 6;
+let resultado = " ";
+
+
+for (index = 0; index < n; index += 1) {
+        resultado += "*";
+}
+for (i = 0; i < n; i += 1) {
+    console.log(resultado);
+} 
+
+
