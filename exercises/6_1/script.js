04deaede32b8f9f7354a00496238131f3b1ce0f8
Daniele Perse <danieleperse3@gmail.com>
2020-08-03 18:06:52 -0300

    adicionei funçao para validar parte dos dados no js

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,4 +1,4 @@
-var estados = ["Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]
+var estados = ["Selecione seu Estado", "Acre", "Alagoas", "Amapá", "Amazonas", "Bahia", "Ceará", "Espírito Santo", "Goiás", "Maranhão", "Mato Grosso", "Mato Grosso do Sul", "Minas Gerais", "Pará", "Paraíba", "Paraná", "Pernambuco", "Piauí", "Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondônia", "Roraima", "Santa Catarina", "São Paulo", "Sergipe", "Tocantins"]
 
 function criarListaDeEstados(estados) {
     for (let i = 0; i < estados.length; i += 1) {
@@ -8,9 +8,21 @@ function criarListaDeEstados(estados) {
         document.getElementById("estado").add(option);
     }
 }
- 
+
+
+function validarDados(event) {
+    const arrayDataInicio = document.querySelector("#dataInicio").value.split("/");
+    if((arrayDataInicio.length !== 3) || (arrayDataInicio[0] <= 0 || arrayDataInicio[0] > 31) || (arrayDataInicio[1] <= 0 || arrayDataInicio[1] > 12) || (arrayDataInicio[2] < 0)) {
+        event.preventDefault();
+        alert("data inválida, por favor, informar no formato DD/MM/AAAA");
+    }
+}
 
 window.onload = function () {
     criarListaDeEstados(estados);
+
+    const botao = document.getElementById('enviar');
+    botao.addEventListener('click', validarDados);
+
 }
 ;
