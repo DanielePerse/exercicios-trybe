2dba5252c48cf0901fa314c47fc4fe35b939f38c
Daniele Perse <danieleperse3@gmail.com>
2020-08-03 23:01:17 -0300

    montando funçao para submeter dados para uma div

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -9,28 +9,54 @@ function criarListaDeEstados(estados) {
     }
 }
 
-function validarData(event) {
+function validarData() {
     const arrayDataInicio = document.querySelector("#dataInicio").value.split("/");
     if((arrayDataInicio.length !== 3) || (arrayDataInicio[0] <= 0 || arrayDataInicio[0] > 31) || (arrayDataInicio[1] <= 0 || arrayDataInicio[1] > 12) || (arrayDataInicio[2] < 0)) {
-        event.preventDefault();
         alert("data inválida, por favor, informar no formato DD/MM/AAAA");
     }
 }
 
-function validarCpf(event) {
+function validarCpf() {
     const cpf = document.querySelector("#cpf").value;
     if (cpf.length !== 11) {
-        alert("Por favor, digitar somente números.")
+        alert("Por favor, digitar somente números.");
     }
+}
+
+
+function validarDados(event) { 
+    event.preventDefault();
+    
+    const nome = document.getElementById("nome").value;
+    const eMail = document.getElementById("e-mail").value;
+    const cpf = document.getElementById("cpf").value;
+    validarCpf (cpf);
+    const endereco = document.getElementById("endereco").value;
+    const cidade = document.getElementById("cidade").value;
+    const estado = document.getElementById("estado").value;
+    const resumoCurriculo = document.getElementById("resumo").value;
+    const cargo = document.getElementById("cargo").value;
+    const descricaoCargo = document.getElementById("descricaoCargo").value;
+    const dataInicio = document.getElementById("dataInicio").value;
+    validarData (dataInicio);
+
+    
+
+    const dadosGerais = [nome, eMail, cpf, endereco, cidade, estado, resumoCurriculo, cargo, descricaoCargo, dataInicio];
+
+    for (let index = 0; index < dadosGerais.length; index += 1) {
+        const dadosConsolidados = document.getElementById("dadosConsolidados");
+        const novoElemento = document.createElement("p");
+        novoElemento.innerHTML = dadosGerais[index];
+        dadosConsolidados.appendChild(novoElemento);
+    }    
 }
 
 window.onload = function () {
     criarListaDeEstados(estados);
 
     const botao = document.getElementById('enviar');
-    botao.addEventListener('submit', validarData);
-    botao.addEventListener('submit', validarCpf);
-    botao.addEventListener('submit', demaisDados);
+    botao.addEventListener('submit', validarDados);
 
 }
 ;
