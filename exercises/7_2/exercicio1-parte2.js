1043823310ea8ed793a448fcbbcebf3f88eb4cb8
Daniele Perse <danieleperse3@gmail.com>
2020-08-11 19:17:46 -0300

    corrigindo arquivos github

---
+++ exercises/7_2/exercicio1-parte2.js
@@ -0,0 +1,31 @@
+const lesson1 = {
+materia: 'Matemática',
+numeroEstudantes: 20,
+professor: 'Maria Clara',
+turno: 'manhã',
+};
+
+const lesson2 = {
+materia: 'História',
+numeroEstudantes: 20,
+professor: 'Carlos',
+};
+
+const lesson3 = {
+materia: 'Matemática',
+numeroEstudantes: 10,
+professor: 'Maria Clara',
+turno: 'noite',
+};
+
+
+const novoTurno = (objeto, chave, valor) => {
+  objeto[chave] = valor;
+  console.log(objeto);
+}
+novoTurno(lesson2, "turno", "manhã");
+
+const listarKeys = (objeto) => {
+  console.log(Object.keys(objeto));
+}
+listarKeys(lesson1);
