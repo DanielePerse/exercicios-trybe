22fdb9262ed8f80a5ecd11fd745a4fab0473f335
Daniele Perse <danieleperse3@gmail.com>
2020-08-13 13:24:38 -0300

    adicionado comentario exercicio 1

--- exercises/7_3/exercicio1.js
+++ exercises/7_3/exercicio1.js
@@ -22,3 +22,7 @@ assert.throws(
   function (){
     sum(4, '5')
   }, /^Error: parameters must be numbers$/);
+
+  //linha 24 foi usado regex no gabarito, conforme esclarecido na aula ao vivo, ele é listado desa maneira
+  // /^ iniciar com esses caracteres e $/ terminar com esses. Pesquisar mais sobre.
+  // mas poderia lançar apenas /Error: parameters must be numbers/
