69bb881fae6e3b851d53b251c1c50cd66a3db6e2
Daniele Perse <danieleperse3@gmail.com>
2020-08-28 10:41:01 -0300

    exercicios 1 e 2 dia 9.2

--- exercises/9_2/apiScript.js
+++ exercises/9_2/apiScript.js
@@ -1,9 +1,45 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
+/* EXERCICIO 1
+  const fetchJoke = () => {
   fetch(API_URL, { headers: { Accept: 'application/json' } })
     .then(response => response.json())
     .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
-};
+};*/
 
-window.onload = () => fetchJoke();
+// EXERCICIO 2
+const fetchPromise = () => {
+
+  const newPromise = new Promise(function(resolve, reject) {
+
+    //produzir array com 10 números aleatórios de 1 até 50;
+    let array = [];
+    for (let i = 0; i < 10; i += 1) {
+      array.push(Math.floor(Math.random() * 51));
+    }
+
+    //elevar numéros do array ao quadrado
+    const quadradoDoArray = array.map((currentValue) => currentValue * currentValue);
+
+    //somar todos os números
+    const somaArray = quadradoDoArray.reduce((acc, currentValue) => acc + currentValue);
+    console.log(somaArray);
+
+    //se <8000 resolve, se >= 8000 reject
+    if (somaArray < 8000) {
+      resolve();
+    } else {
+      reject();
+    }
+    
+  });
+
+  newPromise
+    .then((response) => console.log("resultado dentro de 8000"))
+    .catch((error) => console.log("valor ficou >= 8000"));
+
+}
+fetchPromise();
+
+
+// window.onload = () => fetchJoke();
