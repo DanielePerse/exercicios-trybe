e663d588f564ee11f707051ea8e13fbe5550eaaa
Daniele Perse <danieleperse3@gmail.com>
2020-09-04 15:54:46 -0300

    exercicio2

--- exercises/10_3/exercicio1.test.js
+++ exercises/10_3/exercicio1.test.js
@@ -1,9 +1,18 @@
 const service = require('./exercicio1');
 
-test('Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada', () => {
+test('Exercicio 1: Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada', () => {
   service.aleatoryNumbers = jest.fn().mockReturnValue(10);
 
   expect(service.aleatoryNumbers()).toBe(10);
   expect(service.aleatoryNumbers).toHaveBeenCalled();
   expect(service.aleatoryNumbers).toHaveBeenCalledTimes(1);
-})
+});
+
+
+test('Exercicio 2: Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada', () => {
+  service.aleatoryNumbers = jest.fn().mockImplementationOnce((a, b) => a / b);
+
+  expect(service.aleatoryNumbers(30, 2)).toBe(15);
+  expect(service.aleatoryNumbers).toHaveBeenCalled();
+  expect(service.aleatoryNumbers).toHaveBeenCalledTimes(1);
+});
