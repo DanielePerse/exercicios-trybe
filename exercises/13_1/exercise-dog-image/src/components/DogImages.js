5554063039e12b261ac78d66252898ccff60d260
danieleperse <danieleperse3@gmail.com>
2020-09-22 14:16:25 -0300

    exercicio 1 ok - acesso API - Loading... na tela - botao ok

---
+++ exercises/13_1/exercise-dog-image/src/components/DogImages.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class DogImages extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log("construtor");
+
+    this.state = {
+      imageDoguinho: "",
+      isLoading: true
+    }
+
+    this.fetchImage = this.fetchImage.bind(this);
+  }
+
+  //a requisição deve ser assincrona! async await (alterar isso depois)
+  fetchImage() {
+    this.setState({ isLoading: true }, () => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(response =>
+        this.setState({ imageDoguinho: response.message, isLoading: false }))
+    })
+  }
+
+  //essa funcao irá executar isso quando o componente esta sendo montado - nao da tempo para o usuario fazer outra acao. A imagem do dog vai aparecer primeiro na tela. Ou seja, enquanto estiver montando um componente a imagem de um dog sera mostrada.
+  componentDidMount() {
+    console.log("componenteDidMount");
+    this.fetchImage();
+  }
+
+  render () {
+    const {isLoading} = this.state
+
+    console.log("render")
+    return (
+      <div>
+        <h1>Doguinhos fofíneos</h1>
+        {isLoading ? <p>Loading...</p> : <img src={this.state.imageDoguinho} alt="imagem doguinho" /> }
+        <button onClick={this.fetchImage}>Required New Dog</button>
+      </div>
+    )
+  }
+}
+
+export default DogImages;
