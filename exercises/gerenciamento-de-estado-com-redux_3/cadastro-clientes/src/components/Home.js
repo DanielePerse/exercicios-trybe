51e2110cd71614f0b4dda585804a2fa07fa04b35
danieleperse <danieleperse3@gmail.com>
2020-10-26 19:00:32 -0300

    salvando ultimas atualizaçoes

---
+++ exercises/gerenciamento-de-estado-com-redux_3/cadastro-clientes/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Página de cadastro de clientes</h1>
+        <Link></Link>
+      </div>
+    )
+  }
+}
+
+export default Home;
