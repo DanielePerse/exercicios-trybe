ba8ae85e4e88746f478a512aa021f6cbd2434edf
danieleperse <danieleperse3@gmail.com>
2020-10-27 09:39:17 -0300

    refatorando o target do form

--- exercises/gerenciamento-de-estado-com-redux_3/form-react-redux/src/Form.js
+++ exercises/gerenciamento-de-estado-com-redux_3/form-react-redux/src/Form.js
@@ -15,7 +15,7 @@ class Form extends React.Component {
           <div>
             <label>
               Está ok?
-            <input name="isOk" type="checkbox" value={value.isOk} onChange={(event) => addAssignment(event.target.name, event.target.value)} required></input>
+            <input name="isOk" type="checkbox" required value={value.isOk} onChange={({ target }) => addAssignment(target.name, target.value)}></input>
             </label>
           </div>
 
@@ -26,21 +26,21 @@ class Form extends React.Component {
           <div>
             <label>
               Nota do conteúdo
-            <input name="notaConteudo" type="number" value={value.notaConteudo} onChange={(event) => addAssignment(event.target.name, event.target.value)} required/>
+            <input name="notaConteudo" type="number" value={value.notaConteudo} onChange={({ target }) => addAssignment(target.name, target.value)} required/>
             </label>
           </div>
 
           <div>
             <label>
               O que mais gostou
-            <input name="maisGostou" type="text" value={value.maisGostou} onChange={(event) => addAssignment(event.target.name, event.target.value)} required />
+            <input name="maisGostou" type="text" value={value.maisGostou} onChange={({ target }) => addAssignment(target.name, target.value)} required />
             </label>
           </div>
 
           <div>
             <label>
               Assuntos que teve mais dificuldade
-              <select name="assuntosDificieis" value={value.assuntosDificieis} onChange={(event) => addAssignment(event.target.name, event.target.value)} required >
+              <select name="assuntosDificieis" value={value.assuntosDificieis} onChange={({ target }) => addAssignment(target.name, target.value)} required >
                 <option value="selecione">Selecione</option>
                 <option value="props">Props</option>
                 <option value="estado">Estado</option>
