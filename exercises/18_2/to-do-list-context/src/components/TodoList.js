917ae6a15a3bd8fb3da0779cea384086bdaa8162
danieleperse <danieleperse3@gmail.com>
2020-11-15 10:59:13 -0300

    alterando todolist

--- exercises/18_2/to-do-list-context/src/components/TodoList.js
+++ exercises/18_2/to-do-list-context/src/components/TodoList.js
@@ -24,7 +24,8 @@ import TodoContext from '../context/TodoContext';
 
 //refatoração 3: usando reactHook.
 //1. primeiro importo o "useContext";
-//2. 
+//2. não preciso mais receber a informação de outro componente via props
+//3. não preciso mais do consumer. Então, vou cirar uma variável ("todos") e ele vai vir do meu useContext, e esse useContext deve receber o cntexto ao qual estou me referindo.
 function TodoList() {
   const { todos } = useContext(TodoContext);
 
