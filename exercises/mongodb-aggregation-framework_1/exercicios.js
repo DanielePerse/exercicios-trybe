0938cc65d02bcf7ca89ee0956a0bfbbe8b2af4d3
danieleperse <danieleperse3@gmail.com>
2021-01-25 10:44:36 -0300

    arquiv de exercicios criado e bancos importados

---
+++ exercises/mongodb-aggregation-framework_1/exercicios.js
@@ -0,0 +1,37 @@
+//exercicio 1
+
+
+//exercicio 2
+
+
+//exercicio 3
+
+
+//exercicio 4
+
+
+//exercicio 5
+
+
+//exercicio 6
+
+
+//exercicio 7
+
+
+//exercicio 8
+
+
+//exercicio 9
+
+
+//exercicio 10
+
+
+//exercicio 11
+
+
+//exercicio 12
+
+
+//exercicio 13
