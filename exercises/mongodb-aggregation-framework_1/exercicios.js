2f3222807769f39b1d12618ef25abb36c14fbf6b
danieleperse <danieleperse3@gmail.com>
2021-01-25 18:35:19 -0300

    exercicios dia 25.1 finalizados

--- exercises/mongodb-aggregation-framework_1/exercicios.js
+++ exercises/mongodb-aggregation-framework_1/exercicios.js
@@ -239,18 +239,53 @@ db.vendas.aggregate([
   }}
 ]);
 
-//exercicio 12 - Descubra quais as três uf s que mais compraram no ano de 2020 . Retorne os documentos no seguinte formato:
+//exercicio 12
+db.vendas.aggregate([
+  { $match: {
+    dataVenda: {
+      $gte: ISODate("2020-01-01"),
+      $lte: ISODate("2020-12-31")
+    }
+  } },
+  { $lookup: {
+    localField: "clienteId",
+    foreignField: "clienteId",
+    from: "clientes",
+    as: "dadosClientes"
+  }},
+  { $unwind: "$dadosClientes"},
+  { $group: {
+    _id: "$dadosClientes.endereco.uf",
+    totalVendas: { $sum: 1 }
+  } },
+  { $sort: { totalVendas: -1 }},
+  { $limit: 3 },
+  { $project: {
+    _id: 0,
+    uf: "$_id",
+    totalVendas: 1
+  }}
+]);
 
-{
-  "totalVendas": 10,
-  "uf": "SP"
-}
-
-
-//exercicio 13 - Encontre qual foi o total de vendas e a média de vendas de cada uf no ano de 2019 . Ordene os resultados pelo nome da uf . Retorne os documentos no seguinte formato:
-
-{
-  "_id": "MG",
-  "mediaVendas": 9407.129225352113,
-  "totalVendas": 142
-}
+//exercicio 13
+db.vendas.aggregate([
+  { $match: {
+    dataVenda: {
+      $gte: ISODate("2019-01-01"),
+      $lte: ISODate("2019-12-31")
+    }
+  }},
+  { $lookup: {
+    localField: "clienteId",
+    foreignField: "clienteId",
+    from: "clientes",
+    as: "dadosClientes"
+  }},
+  { $unwind: "$dadosClientes" },
+  { $group: {
+    _id: "$dadosClientes.endereco.uf",
+    mediaVendas: { $avg: "$valorTotal" },
+    totalVendas: { $sum: 1 }
+  }},
+  { $sort: { _id: 1 }}
+]);
