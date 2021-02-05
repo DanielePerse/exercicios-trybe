f86830327ec9b2efa25cbf3b304e691728cfed03
danieleperse <danieleperse3@gmail.com>
2021-02-05 09:36:18 -0300

    exercicio dia 26.1

--- exercises/nodejs_1/calcula-imc/index.js
+++ exercises/nodejs_1/calcula-imc/index.js
@@ -1,44 +1,38 @@
-let inquirer = require('inquirer');
+const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
-    'Informe seu peso:'
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else when wrong
-    }
-  });
-
-let peso = readlineSync.questionFloat('Informe seu peso:');
-let altura = readlineSync.questionFloat('Informe sua altura:');
-
-function calculoIMC(peso, altura) {
-  return (peso / (altura * altura)).toFixed(2)
+function validateFloat (input) {
+  return !isNaN(parseFloat(input)) || 'Por favor, digite um número válido';
 }
 
-let imc = calculoIMC(peso, altura);
+async function calculoIMC() {
+  const result = await inquirer.prompt([
+    { name: 'peso', type: 'input', message: 'Informe seu peso:', validate: validateFloat },
+    { name: 'altura', type: 'input', message: 'Informe sua altura:', validate: validateFloat }
+  ]);
+  const peso = parseFloat(result.peso);
+  const altura = parseFloat(result.altura);
 
-function situacaoIMC(imc) {
+  console.log('Peso: %s, altura: %s', peso, altura);
+  
+  const imc = (peso / Math.pow(altura, 2)).toFixed(2);
+
+  console.log('IMC: %s', imc);
+
   if (imc < 18.5) {
-    return "Abaixo do peso (magreza)"
+    return console.log("Abaixo do peso (magreza)")
   } else if (imc >= 18.5 && imc <= 24.9) {
-    return "Peso normal"
+    return console.log("Peso normal")
   } else if (imc >= 25 && imc <= 29.9) {
-    return "Acima do peso (sobrepeso)"
+    return console.log("Acima do peso (sobrepeso)")
   } else if (imc >= 30 && imc <= 34.9) {
-    return "Obesidade grau I"
+    return console.log("Obesidade grau I")
   } else if (imc >= 35 && imc <= 39.9) {
-    return "Obesidade grau II"
+    return console.log("Obesidade grau II")
   } else {
-    return "Obesidade graus III e IV"
+    return console.log("Obesidade graus III e IV")
   }
+
 }
 
-console.log(calculoIMC(peso, altura))
-console.log(situacaoIMC(imc))
+calculoIMC()
+
