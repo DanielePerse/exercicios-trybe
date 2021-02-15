e9bf82093dcc143f8e629a0aea41dec2a73ac53f
danieleperse <danieleperse3@gmail.com>
2021-02-15 18:46:41 -0300

    crud cirado utilizando banco de dados sql

--- exercises/27_1_nodeJS/index.js
+++ exercises/27_1_nodeJS/index.js
@@ -12,8 +12,47 @@ app.post('/user', async (req, res) => {
   const isValid = Users.isValid(firstName, lastName, email, password);
 
   if (!isValid) return res.status(400).json({ message: 'Dados Inválidos' });
+
   await Users.createUser(firstName, lastName, email, password);
+
   return res.status(201).json({ message: 'Usuário criado com sucesso' });
+});
+
+app.get('/user', async (req, res) => {
+  const getAllUsers = await Users.allUsers();
+
+  return res.status(200).json(getAllUsers);
+});
+
+app.get('/user/:id', async (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = await Users.userById(id);
+
+  if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+  return res.status(200).json(user);
+});
+
+app.put('/user/:id', async (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, email, password } = req.body;
+
+  const isValid = Users.isValid(firstName, lastName, email, password);
+
+  if (!isValid) return res.status(400).json({ message: 'Dados inválidos' });
+
+  await Users.editUser(id, firstName, lastName, email, password);
+
+  return res.status(200).json({ message: 'Usuário editado com sucesso' });
+});
+
+app.delete('/user/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await Users.deleteUser(id);
+
+  return res.status(200).json({ message: 'Usuário deletado com sucesso' });
 });
 
 app.listen(PORT, () => {
--- exercises/27_1_nodeJS/models/usersModel.js
+++ exercises/27_1_nodeJS/models/usersModel.js
@@ -16,7 +16,48 @@ const createUser = async (firstName, lastName, email, password) => {
   );
 };
 
+const allUsers = async () => {
+  const [users] = await connection.execute(
+    'SELECT * FROM users_crud.users;',
+  );
+
+  return users.map(({ id, first_name, last_name, email, password }) => ({
+    id,
+    firstName: first_name,
+    lastName: last_name,
+    email,
+    password,    
+  }));
+}
+
+const userById = async (id) => {
+  const [user] = await connection.execute('SELECT * FROM users_crud.users WHERE id = ?;', [id]);
+
+  if (!user) return null;
+
+  return {
+    id: user[0].id,
+    firstName: user[0].first_name,
+    lastName: user[0].last_name,
+    email: user[0].email,
+    password: user[0].password,
+  };
+};
+
+const editUser = async (id, firstName, lastName, email, password) => {
+  connection.execute('UPDATE users_crud.users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ?;',
+  [firstName, lastName, email, password, id]);
+};
+
+const deleteUser = async (id) => {
+  connection.execute('DELETE FROM users_crud.users WHERE id = ?;', [id]);
+};
+
 module.exports = {
   isValid,
   createUser,
+  allUsers,
+  userById,
+  editUser,
+  deleteUser,
 };
