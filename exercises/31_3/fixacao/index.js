3afb2a3c8011819a9d2944c75f1e9406931de964
danieleperse <danieleperse3@gmail.com>
2021-03-29 15:50:34 -0300

    utilizando metodo eager loading - criando mais uma rota no index

--- exercises/31_3/fixacao/index.js
+++ exercises/31_3/fixacao/index.js
@@ -8,5 +8,18 @@ app.get('/employees', (_req, res) => Employees
   .then((answer) => res.status(200).json(answer))
   .catch(() => res.status(500).json({ message: 'Algo deu errado' })));
 
+app.get('/employees/:id', (req, res) => Employees
+.findAll({
+  where: { employee_id: req.params.id },
+  include: [{ model: Addresses, as: 'addresses' }],
+})
+.then((employee) => {
+  if (!employee.length) {
+    return res.status(404).send({ message: 'Funcionário não encontrado' });
+  }
+  return res.status(200).json(employee);
+})
+.catch(() => res.status(500).json({ message: 'Algo deu errado' })));
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Port: ${PORT}`));
