ba903d27a4ded8c95ba7c77667ef7aa5bbf8baf6
danieleperse <danieleperse3@gmail.com>
2021-03-29 19:57:17 -0300

    exercicios do dia finalizados

--- exercises/31_3/exercicio-do-dia/index.js
+++ exercises/31_3/exercicio-do-dia/index.js
@@ -1,4 +1,3 @@
-
 const bodyParser = require('body-parser');
 
 const express = require('express');
@@ -8,6 +7,17 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 3000;
+
+const { getAllPatientsPlans, getAllPatientsSurgeries, createPatients, getPatientsAndSurgeriesNoDoctor } = require('./controllers/patientsController');
+const getAllPlans = require('./controllers/plansController');
+const getDoctorSurgeries = require('./controllers/surgeriesController');
+
+app.post('/', createPatients);
+app.get('/all', getAllPatientsPlans);
+app.get('/surgeries', getAllPatientsSurgeries);
+app.get('/surgeries/nodoctor', getPatientsAndSurgeriesNoDoctor);
+app.get('/surgeries/:name', getDoctorSurgeries);
+app.get('/:id', getAllPlans);
 
 app.listen(PORT, () => {
   console.log(`Port: ${PORT}`);
