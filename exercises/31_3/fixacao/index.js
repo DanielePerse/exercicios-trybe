a42a9133401683a0aba8ec9522dff841307359ce
danieleperse <danieleperse3@gmail.com>
2021-03-29 17:21:35 -0300

    add books ao banco - testes co relacionamento N:N

--- exercises/31_3/fixacao/index.js
+++ exercises/31_3/fixacao/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Addresses, Employees } = require('./models');
+const { Books, Users } = require('./models');
 
 const app = express();
 
@@ -23,6 +24,19 @@ app.get('/employees/:id', (req, res) => Employees
     return res.status(200).json(employee);
   })
   .catch(() => res.status(500).json({ message: 'Algo deu errado' })));
+
+app.get('/usersbooks/:id', (req, res) => Users
+.findAll({
+  where: { user_id: req.params.id },
+  include: [{ model: Books, as: 'books', through: { attributes: [] } }],
+})
+.then((usersbooks) => {
+  if (!usersbooks.length) {
+    return res.status(404).send({ message: 'Usuário não encontrado' });
+  }
+  return res.status(200).json(usersbooks);
+})
+.catch(() => res.status(500).json({ message: 'Algo deu errado' })));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Port: ${PORT}`));
