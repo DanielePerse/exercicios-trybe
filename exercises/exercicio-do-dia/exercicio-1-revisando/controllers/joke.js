b7e6070e4c8699c63d6d014989b444a3ed956cb9
danieleperse <danieleperse3@gmail.com>
2021-04-09 15:24:41 -0300

    exercicio 1 - controller corrigido

--- exercises/exercicio-do-dia/exercicio-1-revisando/controllers/joke.js
+++ exercises/exercicio-do-dia/exercicio-1-revisando/controllers/joke.js
@@ -1,10 +1,7 @@
 const jokeModel = require('../models/joke');
 
 const listJokes = async (req, res) => {
-  console.log('cheguei no controller 0')
   const joke = await jokeModel.getJokes();
-  console.log(joke)
-  console.log('cheguei no controller')
 
   return res.render('jokeView', { joke });
 };
