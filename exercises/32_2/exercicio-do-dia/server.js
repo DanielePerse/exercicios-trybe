91009c38a4c5ff5d33c489965b556060491aa42c
danieleperse <danieleperse3@gmail.com>
2021-04-12 18:17:39 -0300

    exercicio 1

--- exercises/32_2/exercicio-do-dia/server.js
+++ exercises/32_2/exercicio-do-dia/server.js
@@ -1,14 +1,16 @@
 const net = require('net');
 
+// exercicio 1: experimentem remover a resposta do servidor e veja quanto tempo a conexão fica ligada! Para isso, basta comentar o trecho de código.
+// ao fazer isso, a porta não desconecta
 const server = net.createServer((connection) => {
-  console.log('Cliente conectado');
+  // console.log('Cliente conectado');
 
-  connection.on('end', () => {
-    console.log('Cliente desconectado');
-  });
+  // connection.on('end', () => {
+  //   console.log('Cliente desconectado');
+  // });
 
-  connection.write('Mensagem do servidor!\r\n');
-  connection.pipe(connection);
+  // connection.write('Mensagem do servidor!\r\n');
+  // connection.pipe(connection);
 });
 
 server.listen(8080, () => {
