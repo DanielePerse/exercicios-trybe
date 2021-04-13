22624c51f497b9033af398d9ccf2edd1ff53c58f
danieleperse <danieleperse3@gmail.com>
2021-04-13 10:32:13 -0300

    socket io sendo implementado

--- exercises/32_3/fixacao/index.js
+++ exercises/32_3/fixacao/index.js
@@ -1,8 +1,24 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  }
+});
+
+app.use(cors()) // Permite recursos restritos na página web serem pedidos a domínio externo
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log(
+    'Usuário conectado, igual ao que fizemos na aula anterior, porém dessa vez em um servidor escalável'
+  );
 });
 
 http.listen(3000, () => {
