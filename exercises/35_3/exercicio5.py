642ada961c339973170247b88348edc154433c25
danieleperse <danieleperse3@gmail.com>
2021-05-12 15:41:39 -0300

    exerc 5

--- exercises/35_3/exercicio5.py
+++ exercises/35_3/exercicio5.py
@@ -7,14 +7,18 @@ response = requests.get(
 )
 
 selector = Selector(response.text)
+
+
 title = selector.css("h1::text").get()
 price = selector.css(".price_color::text").get()[2:]
 description = selector.css("#product_description ~ p::text").get()
 srcImage = selector.css("#product_gallery img::attr(src)").get()
 urlImage = "http://books.toscrape.com/catalogue/" + srcImage
+availableBooks = selector.css(".product_main .availability::text").re_first(
+    "\d"
+)
 
-
-print(title, price, description, urlImage, sep=" - ")
+print(title, price, description, urlImage, availableBooks, sep=" - ")
 
 
 # título, preço, descrição e url contendo a imagem de capa do livro
